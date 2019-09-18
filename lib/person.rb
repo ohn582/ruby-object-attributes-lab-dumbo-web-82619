e251efbd986b954ed49887da9732f6d88da2fd7e
Neal JIn Oh <no4259@gmail.com>
2019-09-18 03:17:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
- 
+ def name= (new_name)
+    @name = new_name
+  end
+  
+  def name
+    @name
+  end
+  
+  
+  
+  def job= (new)
+    @job = new
+  end
+  
+  def job
+    @job
+  end
 end
